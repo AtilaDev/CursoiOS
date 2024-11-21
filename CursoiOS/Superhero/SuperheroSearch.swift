656ac8ac5fa09b75dca3538247fd91e93ca60dd8
Leandro Favre <atiladevelop@gmail.com>
2024-11-21 03:40:07 -0300

    Working with images from fetching

--- CursoiOS/Superhero/SuperheroSearch.swift
+++ CursoiOS/Superhero/SuperheroSearch.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct SuperheroSearch: View {
   @State var superheroName:String = ""
+  @State var wrapper:ApiNetwork.Wrapper? = nil
+  
   var body: some View {
     VStack{
       TextField("", text: $superheroName, prompt:
@@ -27,21 +29,51 @@ struct SuperheroSearch: View {
         print(superheroName)
         Task{
           do{
-            let response = try await ApiNetwork().getHeroesByQuery(query: superheroName)
-            print(response)
+            wrapper = try await ApiNetwork()
+              .getHeroesByQuery(query: superheroName)
           }catch{
             print("Error")
           }
         }
       }
-      Spacer()
+      List(wrapper?.results ?? []){ superhero in
+        SuperheroItem(superhero: superhero)
+      }
+      .listStyle(.plain)
       
+      Spacer()
     }
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .background(.backgroundApp)
   }
 }
 
+struct SuperheroItem: View {
+  let superhero:ApiNetwork.Superhero
+  
+  var body: some View {
+    ZStack{
+      Rectangle()
+      VStack{
+        Spacer()
+        Text(superhero.name)
+          .foregroundColor(.white)
+          .font(.title)
+          .bold()
+          .padding()
+          .frame(maxWidth: .infinity)
+          .background(.white.opacity(0.5))
+      }
+    }
+    .frame(height: 200)
+    .cornerRadius(32)
+    .listRowBackground(Color.backgroundApp)
+  }
+}
+
+
+
 #Preview {
+//  SuperheroItem(superhero: ApiNetwork.Superhero(id: "", name: "Ary"))
   SuperheroSearch()
 }
