aea1911e5dae77d381328589f1d2bf7dae54a89f
Leandro Favre <atiladevelop@gmail.com>
2024-11-23 11:53:50 -0300

    savePlaces func done

--- CursoiOS/FavPlaces/FavPlaces.swift
+++ CursoiOS/FavPlaces/FavPlaces.swift
@@ -18,7 +18,8 @@ struct FavPlaces: View {
   )
   
   @State var places:[Place] = []
-  @State var showPopUp:Bool = false
+//  @State var showPopUp:Bool = false
+  @State var showPopUp:CLLocationCoordinate2D? = nil
   @State var name:String = ""
   @State var favPlace:Bool = false
   
@@ -26,11 +27,13 @@ struct FavPlaces: View {
     ZStack{
       MapReader{ proxy in
         Map(position: $position)
-          .onTapGesture {
-            showPopUp = true
+          .onTapGesture { coords in
+            if let coordinates = proxy.convert(coords, from: .local){
+              showPopUp = coordinates
+            }
           }
       }
-      if showPopUp {
+      if showPopUp != nil {
         
         let view = VStack{
           Text("Añadir localización").font(.title2).bold().padding(.bottom, 16)
@@ -40,16 +43,21 @@ struct FavPlaces: View {
             .cornerRadius(16)
           Toggle("¿Es un lugar favorito?", isOn: $favPlace).padding(.bottom, 24)
           Button("Guardar") {
-            
+            savePlace(name: name, fav: favPlace, coordinates: showPopUp!)
           }
         }
         withAnimation{
           CustomDialog(closeDialog: {
-            showPopUp = false
+            showPopUp = nil
           }, onDismissOutside: false, content: view)
         }
       }
     }
+  }
+  
+  func savePlace(name:String, fav:Bool, coordinates: CLLocationCoordinate2D){
+    let place = Place(name: name, coordinates: coordinates, fav: favPlace)
+    places.append(place)
   }
 }
 
