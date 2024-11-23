edf303ec7666c1129a173fdc9956f8736b944a5f
Leandro Favre <atiladevelop@gmail.com>
2024-11-22 21:19:38 -0300

    Models settings

--- CursoiOS/FavPlaces/FavPlaces.swift
+++ CursoiOS/FavPlaces/FavPlaces.swift
@@ -9,13 +9,27 @@ import SwiftUI
 import MapKit
 
 struct FavPlaces: View {
-    var body: some View {
-      ZStack{
-        Map()
+  
+  @State var position = MapCameraPosition.region(
+    MKCoordinateRegion(
+      center: CLLocationCoordinate2D(latitude: 36.755306 , longitude: -4.424076),
+      span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+    )
+  )
+  
+  var body: some View {
+    ZStack{
+      MapReader{ proxy in
+        Map(position: $position)
+          .onTapGesture {
+//
+          }
+        
       }
     }
+  }
 }
 
 #Preview {
-    FavPlaces()
+  FavPlaces()
 }
---
+++ CursoiOS/FavPlaces/models/Place.swift
@@ -0,0 +1,8 @@
+//
+//  Place.swift
+//  CursoiOS
+//
+//  Created by Leandro Favre on 22/11/2024.
+//
+
+import Foundation
